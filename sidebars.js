325094c67bbcefcc393ca07284eed5d512acad1b
caradragos <dragos@flowx.ai>
2023-01-09 12:11:49 +0200

    changes

--- sidebars.js
+++ sidebars.js
@@ -106,11 +106,7 @@ const sidebars = {
 
     {
       type: 'html',
-<<<<<<< HEAD
       value: '<b>PLATFORM SETUP GUIDES</b>', // This is the "Title" item.
-=======
-      value: 'PLATFORM SETUP GUIDES', // This is the "Title" item.
->>>>>>> dae9f68 ( changes)
       className: 'sidebar-title',
     },
   
